a90fa5a2b77c7aeeb875e34b3278f6c83f76aa0c
Tomas Jakubek <tmsjkbk@gmail.com>
2024-09-30 21:44:14 +0200

    feat(ui): increase default tracesSampleRate

--- apps/ui/sentry.client.config.ts
+++ apps/ui/sentry.client.config.ts
@@ -18,10 +18,10 @@ Sentry.init({
   ],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 0.1,
+  tracesSampleRate: 0.3,
 
   // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // This sets the sample rate to be 0%. You may want this to be 100% while
   // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0,
 
--- apps/ui/sentry.edge.config.ts
+++ apps/ui/sentry.edge.config.ts
@@ -10,7 +10,7 @@ Sentry.init({
   dsn: env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 0.1,
+  tracesSampleRate: 0.3,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
