6853f0f55a1c4e10b780451e742f702e86d64c0b
Dominik Juriga <dominik.juriga@notum.cz>
2025-04-30 10:53:34 +0200

    chore: remore use client from table component

--- apps/ui/src/components/ui/table.tsx
+++ apps/ui/src/components/ui/table.tsx
@@ -1,8 +1,6 @@
-"use client"
-
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/styles"
 
 function Table({ className, ...props }: React.ComponentProps<"table">) {
   return (
