4cc5529ebe03906f5206ed576284bd7917224e49
Tomas Jakubek <tmsjkbk@gmail.com>
2025-05-04 17:12:56 +0200

    feat(ui): add DELETE and HEAD methods to proxy handler

--- apps/ui/src/app/api/proxy/[...slug]/route.ts
+++ apps/ui/src/app/api/proxy/[...slug]/route.ts
@@ -55,4 +55,10 @@ async function handler(
   return response
 }
 
-export { handler as GET, handler as POST, handler as PUT }
+export {
+  handler as GET,
+  handler as POST,
+  handler as PUT,
+  handler as DELETE,
+  handler as HEAD,
+}
