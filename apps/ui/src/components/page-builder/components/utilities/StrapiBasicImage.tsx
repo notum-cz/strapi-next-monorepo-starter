d6c184f8d920e8f4ff2855490c4a4aa5ca5ac249
Tomas Jakubek <tmsjkbk@gmail.com>
2025-05-05 16:05:26 +0200

    feat(ui): enhance StrapiBasicImage component with format and native image support

--- apps/ui/src/components/page-builder/components/utilities/StrapiBasicImage.tsx
+++ apps/ui/src/components/page-builder/components/utilities/StrapiBasicImage.tsx
@@ -1,17 +1,22 @@
+import Image from "next/image"
+
 import type { StrapiImageMedia } from "@/types/api"
 import type { ImageExtendedProps } from "@/types/next"
 import type { Data } from "@repo/strapi"
 
+import { formatStrapiMediaUrl } from "@/lib/strapi-helpers"
 import { ImageWithFallback } from "@/components/elementary/ImageWithFallback"
 
 export interface BasicImageProps
   extends Omit<ImageExtendedProps, "src" | "alt"> {
   readonly component: Data.Component<"utilities.basic-image"> | undefined | null
   readonly useClient?: boolean
+  readonly useNativeNextImageOnly?: boolean
   readonly className?: ImageExtendedProps["className"]
   readonly hideWhenMissing?: boolean
   readonly fallbackSizes?: { width?: number | null; height?: number | null }
   readonly forcedSizes?: { width?: number | null; height?: number | null }
+  readonly format?: "large" | "small" | "medium" | "thumbnail"
 }
 
 export function StrapiBasicImage({
@@ -21,22 +26,30 @@ export function StrapiBasicImage({
   hideWhenMissing,
   fallbackSizes,
   forcedSizes,
+  format,
+  useNativeNextImageOnly,
   ...imgProps
 }: BasicImageProps) {
   const media: StrapiImageMedia = component?.media
+  const selectedFormat = format ? media?.formats?.[format] : undefined
 
-  if (media?.url == null && hideWhenMissing) {
+  const url = selectedFormat?.url ?? media?.url
+
+  if (url == null && hideWhenMissing) {
     return null
   }
 
-  // TODO: fix basic image to in client components
+  const ImageComp = useNativeNextImageOnly ? Image : ImageWithFallback
+
+  // TODO: the placeholder library is not working properly and crashes the storybook - revise its usage
   // const ImageComp = useClient ? ImageWithFallback : ImageWithPlaiceholder
-  const ImageComp = ImageWithFallback
+  // useClient  ? ImageWithFallback : ImageWithPlaiceholder
 
   const sizes = {
     width:
       forcedSizes?.width ??
       component?.width ??
+      selectedFormat?.width ??
       media?.width ??
       fallbackSizes?.width ??
       undefined,
@@ -44,18 +57,36 @@ export function StrapiBasicImage({
     height:
       forcedSizes?.height ??
       component?.height ??
+      selectedFormat?.height ??
       media?.height ??
       fallbackSizes?.height ??
       undefined,
+  }
+
+  const src = formatStrapiMediaUrl(url)
+  const fallbackSrc = formatStrapiMediaUrl(component?.fallbackSrc)
+
+  if (imgProps.fill) {
+    // Fill has priority over sizes
+    sizes.width = undefined
+    sizes.height = undefined
+  }
+
+  if (!imgProps.fill && (!sizes.width || !sizes.height)) {
+    // If width or height is missing, fill the image
+    // This happens mostly if there is no media in Strapi and
+    // we are using a fallback image
+    imgProps.fill = true
+    sizes.width = undefined
+    sizes.height = undefined
   }
 
   return (
     <ImageComp
       style={{ ...sizes, ...imgProps.style }}
       className={className}
-      // @ts-expect-error - src needs to be typed in better way
-      src={media?.url}
-      fallbackSrc={component?.fallbackSrc ?? undefined}
+      src={src}
+      fallbackSrc={fallbackSrc}
       alt={component?.alt ?? media?.caption ?? media?.alternativeText ?? ""}
       {...sizes}
       {...imgProps}
