63e0ba38c9168cb5d461c64580fc4edd2919221c
Tomas Jakubek <tmsjkbk@gmail.com>
2025-05-06 22:53:39 +0200

    fix(ui): fetch all nested components of page content using populateDeep

--- apps/ui/src/lib/content.ts
+++ apps/ui/src/lib/content.ts
@@ -12,21 +12,25 @@ const seoPopulateObj = {
   },
 } as const
 
-export async function fetchPageData(pageUrl: string, locale: AppLocale) {
-  console.log({ message: `Fetching page: ${locale}, ${pageUrl}` })
+export async function fetchPageData(fullPath: string, locale: AppLocale) {
+  console.log({ message: `Fetching page: ${locale} - ${fullPath}` })
 
   try {
-    return await Strapi.fetchOneByFullPath("api::page.page", pageUrl, {
+    return await Strapi.fetchOneByFullPath("api::page.page", fullPath, {
       locale,
       status: "published",
       populate: {
         content: true,
         ...seoPopulateObj,
       },
+      // Use with BIG caution, this can lead to a lot of data being fetched
+      deepLevel: 6,
+      // Ignore these fields when deep populating
+      deepLevelIgnore: ["children", "parent", "localizations", "seo"],
     })
   } catch (e: any) {
     console.error({
-      message: `Error fetching page: ${locale}, ${pageUrl}`,
+      message: `Error fetching page: ${locale} - ${fullPath}`,
       data: JSON.stringify({
         error: e?.message,
         stack: e?.stack,
