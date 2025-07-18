99c00a776db157b2a9d7949a9a690baa8485f821
Ondrej Mikulcik <omikulcik@seznam.cz>
2025-07-18 15:43:54 +0200

    chore(heroku postbuild): remove strapi and node modules cache from slug

--- scripts/heroku/heroku-postbuild.sh
+++ scripts/heroku/heroku-postbuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$APP" == "ui" ]; then
-    yarn build:ui
+    yarn build:ui && yarn build:ui && rm -rf apps/strapi .turbo node_modules/.cache
 elif [ "$APP" == "strapi" ]; then
     yarn build:strapi
 else
