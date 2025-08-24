804126c4918ad91875e7dfb8d057a6e79417ddf2
Tomas Skarpa <tomas.skarpa@bata.com>
2025-08-24 21:17:41 +0200

    refactor: update robots.txt rules structure for improved clarity and organization

--- apps/ui/src/app/robots.ts
+++ apps/ui/src/app/robots.ts
@@ -9,10 +9,17 @@ const baseUrl = env.APP_PUBLIC_URL
 export default function robots(): MetadataRoute.Robots {
   return isProduction()
     ? {
-        rules: {
-          userAgent: "*",
-          allow: "/",
-        },
+        rules: [
+          {
+            userAgent: "*",
+            allow: "/",
+            disallow: ["/admin", "/account"],
+          },
+          {
+            userAgent: ["UbiCrawler", "DOC", "Zao", "sitecheck.internetseer.com", "Zealbot", "MSIECrawler", "SiteSnagger", "WebStripper", "WebCopier", "Fetch", "Offline Explorer", "Teleport", "TeleportPro", "WebZIP", "linko", "HTTrack", "Microsoft.URL.Control", "Xenu", "larbin", "libwww", "ZyBORG", "Download Ninja", "fast", "Maxthon", "CNCDialer", "MJ12bot"],
+            disallow: "/",
+          },
+        ],
         sitemap: new URL("sitemap.xml", baseUrl).toString(),
       }
     : {
