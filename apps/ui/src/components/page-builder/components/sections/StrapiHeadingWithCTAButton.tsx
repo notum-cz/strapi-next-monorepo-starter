7b57d675cb543a8c34eb440fc822026b9c2c719d
Ondřej Chrastina <9218736+Simply007@users.noreply.github.com>
2025-09-02 17:30:26 +0200

    feat(cta with button component): style the component to be ready to use with requred fields set

--- apps/ui/src/components/page-builder/components/sections/StrapiHeadingWithCTAButton.tsx
+++ apps/ui/src/components/page-builder/components/sections/StrapiHeadingWithCTAButton.tsx
@@ -15,20 +15,23 @@ export const StrapiHeadingWithCTAButton = ({
   return (
     <section className="px-4 py-8 sm:py-16 lg:px-6">
       <Container>
-        <div className="mx-auto max-w-(--breakpoint-sm) text-center">
-          <h2 className="mb-4 text-4xl leading-tight font-extrabold tracking-tight text-gray-900">
-            {component.title}
-          </h2>
-          {component.subText && (
-            <p className="mb-6 font-light text-gray-500 md:text-lg">
-              {component.subText}
-            </p>
-          )}
-
-          <StrapiLink
-            component={component.cta}
-            className="focus:ring-primary-300 bg-primary inline-flex items-center justify-center rounded-lg px-5 py-3 text-center text-base font-medium text-white focus:ring-4"
-          />
+        <div className="mx-auto max-w-[--breakpoint-sm] grid grid-cols-1 sm:grid-cols-3 items-center text-left gap-8">
+          <div className="sm:col-span-2 flex flex-col items-center text-center">
+            <h2 className="mb-4 text-4xl leading-tight font-extrabold tracking-tight text-gray-900">
+              {component.title}
+            </h2>
+            {component.subText && (
+              <p className="mb-6 font-light text-gray-500 md:text-lg">
+                {component.subText}
+              </p>
+            )}
+          </div>
+          <div className="sm:col-span-1 flex items-center justify-start h-full self-center">
+            <StrapiLink
+              component={component.cta}
+              className="inline-flex w-full lg:w-fit items-center justify-center rounded-lg bg-neutral-900 px-8 py-4 text-lg font-semibold text-white hover:bg-neutral-800 transition-colors focus:outline-none focus:ring-2 focus:ring-neutral-400"
+            />
+          </div>
         </div>
       </Container>
     </section>
