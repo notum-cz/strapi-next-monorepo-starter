9516c12f512adcdf577d8996aa3d142fceae97fe
Tomas Jakubek <tmsjkbk@gmail.com>
2025-11-25 17:45:28 +0800

    fix(strapi): execute InternalJobs related code only within this collection

--- apps/strapi/src/admin/app.tsx
+++ apps/strapi/src/admin/app.tsx
@@ -7,7 +7,7 @@ import { cs } from "./cs"
 import "@repo/design-system/styles.css"
 
 import { defaultCkEditorConfig } from "./ckeditor/configs"
-import InternalJobsActions from "./extensions/InternalJobs"
+import InternalJobs from "./extensions/InternalJobs"
 
 export default {
   config: {
@@ -18,8 +18,8 @@ export default {
   },
   bootstrap(app: StrapiApp) {
     app.getPlugin("content-manager").injectComponent("listView", "actions", {
-      name: "InternalJobsRunAction",
-      Component: InternalJobsActions,
+      name: "InternalJobs",
+      Component: InternalJobs,
     })
   },
   register() {
