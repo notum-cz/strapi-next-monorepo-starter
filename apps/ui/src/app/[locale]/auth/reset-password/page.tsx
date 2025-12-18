d8d6244cbab0936611ae7e1b0f96a2b5e8f12db5
Patrik Novák <novaak.patrik@gmail.com>
2025-12-18 13:52:17 +0100

    feat: add setRequestLocale to each page and layout

--- apps/ui/src/app/[locale]/auth/reset-password/page.tsx
+++ apps/ui/src/app/[locale]/auth/reset-password/page.tsx
@@ -1,8 +1,18 @@
+import { use } from "react"
+import { Locale } from "next-intl"
+import { setRequestLocale } from "next-intl/server"
+
 import { removeThisWhenYouNeedMe } from "@/lib/general-helpers"
 import { SetPasswordForm } from "@/app/[locale]/auth/activate/_components/SetPasswordForm"
 
-export default function ResetPasswordPage() {
+export default function ResetPasswordPage({
+  params,
+}: PageProps<"/[locale]/auth/reset-password">) {
   removeThisWhenYouNeedMe("ResetPasswordPage")
+
+  const { locale } = use(params) as { locale: Locale }
+
+  setRequestLocale(locale)
 
   return <SetPasswordForm />
 }
