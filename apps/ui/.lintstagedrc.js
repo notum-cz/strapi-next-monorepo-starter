4cd96e14341fd360fee50599ce30692a0a14ab15
Patrik Novák <novaak.patrik@gmail.com>
2025-12-19 14:15:34 +0100

    feat: upgrade next to v16

--- apps/ui/.lintstagedrc.js
+++ apps/ui/.lintstagedrc.js
@@ -1,9 +1,5 @@
-const path = require("path")
-
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`
+  `eslint --fix ${filenames.map((f) => `"${f}"`).join(" ")}`
 
 module.exports = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
