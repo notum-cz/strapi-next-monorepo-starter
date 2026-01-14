4bee4f09f86be15d8373df7b1c4b03abb4c7eebc
Martin Čapek <martincapek@users.noreply.github.com>
2026-01-14 14:39:50 +0100

    feat: update all eslint configurations to Eslint 9

--- apps/ui/eslint.config.mjs
+++ apps/ui/eslint.config.mjs
@@ -1,16 +1,4 @@
-import nextVitals from "eslint-config-next/core-web-vitals"
-import { defineConfig, globalIgnores } from "eslint/config"
+import { nextJsConfig } from "@repo/eslint-config/next-js"
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-])
-
-export default eslintConfig
+/** @type {import("eslint").Linter.Config[]} */
+export default nextJsConfig
