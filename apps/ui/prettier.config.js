5df2739f95788c268aa8d9cca0e673e5f9801d3c
Tomas Jakubek <tmsjkbk@gmail.com>
2026-01-27 00:01:10 +0100

    chore(prettier-config): update prettier plugins to use require.resolve

--- apps/ui/prettier.config.js
+++ apps/ui/prettier.config.js
@@ -3,5 +3,8 @@ const globalConfig = require("@repo/prettier-config")
 /** @type {import('prettier').Config} */
 module.exports = {
   ...globalConfig,
-  plugins: [...globalConfig.plugins, "prettier-plugin-tailwindcss"],
+  plugins: [
+    ...globalConfig.plugins,
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
 }
