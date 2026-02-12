cc0b891b5f7cfab8d9e92176f917abbe1d393def
Libor Říha <libor.riha@notum.cz>
2026-02-12 10:05:34 +0100

    feat:  add components list to template

---
+++ apps/ui/src/app/[locale]/dev/components-overview/components/ComponentsList.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { useState } from "react"
+
+import AppLink from "@/components/elementary/AppLink"
+import Typography from "@/components/typography"
+import { Button } from "@/components/ui/button"
+
+export default function ComponentsList({
+  components,
+  pages,
+}: {
+  components: string[]
+  pages: any[]
+}) {
+  const [show, setShow] = useState(true)
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="my-6">
+        <Button
+          type="button"
+          variant={show ? "default" : "outline"}
+          onClick={() => setShow((prev) => !prev)}
+        >
+          {show ? "Hide pages" : "Show pages"}
+        </Button>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        {components?.map((component) => (
+          <div key={component}>
+            <Typography variant="large">{component}</Typography>
+
+            {show && (
+              <div className="pl-4">
+                {pages
+                  .filter((page) =>
+                    page.content.some(
+                      (block: any) => block.__component === component
+                    )
+                  )
+                  .map((page) => (
+                    <div key={page.documentId}>
+                      <AppLink
+                        href={page.fullPath}
+                        openInNewTab
+                        className="mb-2 p-0"
+                      >
+                        {page.fullPath}
+                      </AppLink>
+                    </div>
+                  ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ apps/ui/src/app/[locale]/dev/pages-overview/page.tsx
@@ -0,0 +1,55 @@
+import { headers } from "next/headers"
+import { notFound } from "next/navigation"
+import { Locale } from "next-intl"
+import { setRequestLocale } from "next-intl/server"
+
+import { isProduction } from "@/lib/general-helpers"
+import { logNonBlockingError } from "@/lib/logging"
+import { PublicStrapiClient } from "@/lib/strapi-api"
+import { Container } from "@/components/elementary/Container"
+import Typography from "@/components/typography"
+import DevNavbar from "@/app/[locale]/dev/components/DevNavbar"
+import PageList from "@/app/[locale]/dev/pages-overview/components/PageList"
+
+async function fetchAllPages(locale: Locale) {
+  try {
+    return await PublicStrapiClient.fetchAll("api::page.page", {
+      locale,
+      populate: { content: true },
+      status: "published",
+    })
+  } catch (e: any) {
+    logNonBlockingError({
+      message: `Error fetching all pages for locale '${locale}'`,
+      error: {
+        error: e?.message,
+        stack: e?.stack,
+      },
+    })
+    return { data: [] }
+  }
+}
+export default async function PagesOverviewPage({
+  params,
+}: PageProps<"/[locale]/dev/pages-overview">) {
+  if (isProduction()) {
+    notFound()
+  }
+
+  const { locale } = (await params) as { locale: Locale }
+  setRequestLocale(locale)
+
+  const response = await fetchAllPages(locale)
+
+  const pages = response?.data ?? []
+
+  return (
+    <>
+      <DevNavbar />
+      <Container className="py-20">
+        <Typography tag="h1">All Pages ({pages?.length})</Typography>
+        <PageList pages={pages} />
+      </Container>
+    </>
+  )
+}
