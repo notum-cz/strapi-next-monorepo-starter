b1f2031f5ab190ff8f13836284839f12f877a7d6
Tomas Jakubek <tmsjkbk@gmail.com>
2026-02-12 22:56:57 +0100

    feat: create pages for static and dynamic rendering

---
+++ apps/ui/src/app/[locale]/dynamic/[[...rest]]/layout.tsx
@@ -0,0 +1,15 @@
+import { use } from "react"
+
+import { type Locale } from "next-intl"
+import { setRequestLocale } from "next-intl/server"
+
+export default function Layout({
+  children,
+  params,
+}: LayoutProps<"/[locale]/[[...rest]]">) {
+  const { locale } = use(params) as { locale: Locale }
+
+  setRequestLocale(locale)
+
+  return <div className="flex items-center pb-8">{children}</div>
+}
---
+++ apps/ui/src/components/layouts/StrapiPageView.tsx
@@ -0,0 +1,84 @@
+import { use } from "react"
+import { notFound } from "next/navigation"
+import { ROOT_PAGE_PATH } from "@repo/shared-data"
+import { type Locale } from "next-intl"
+import { setRequestLocale } from "next-intl/server"
+
+import { fetchPage } from "@/lib/strapi-api/content/server"
+import { cn } from "@/lib/styles"
+import { Breadcrumbs } from "@/components/elementary/Breadcrumbs"
+import { Container } from "@/components/elementary/Container"
+import { ErrorBoundary } from "@/components/elementary/ErrorBoundary"
+import { PageContentComponents } from "@/components/page-builder"
+import StrapiStructuredData from "@/components/page-builder/components/seo-utilities/StrapiStructuredData"
+
+interface Props {
+  params: {
+    locale: string
+    rest?: string[]
+  }
+  searchParams?: { [key: string]: string | string[] | undefined }
+}
+
+export default function StrapiPageView({ params, searchParams }: Props) {
+  const locale = params.locale as Locale
+
+  setRequestLocale(locale)
+
+  const fullPath = ROOT_PAGE_PATH + (params.rest ?? []).join("/")
+  const response = use(fetchPage(fullPath, locale))
+
+  const data = response?.data
+  if (data?.content == null) {
+    notFound()
+  }
+
+  const { content, ...restPageData } = data
+
+  return (
+    <>
+      <StrapiStructuredData structuredData={data?.seo?.structuredData} />
+
+      <main className={cn("flex w-full flex-col overflow-hidden")}>
+        <Container>
+          <Breadcrumbs
+            breadcrumbs={response?.meta?.breadcrumbs}
+            className="mt-6 mb-6"
+          />
+        </Container>
+
+        {content
+          .filter((comp) => comp != null)
+          .map((comp) => {
+            const name = comp.__component
+            const id = comp.id
+            const key = `${name}-${id}`
+            const Component = PageContentComponents[name]
+            if (Component == null) {
+              console.warn(`Unknown component "${name}" with id "${id}".`)
+
+              return (
+                <div key={key} className="font-medium text-red-500">
+                  Component &quot;{key}&quot; is not implemented on the
+                  frontend.
+                </div>
+              )
+            }
+
+            return (
+              <ErrorBoundary key={key}>
+                <div className={cn("mb-4 md:mb-12 lg:mb-16")}>
+                  <Component
+                    component={comp}
+                    pageParams={params}
+                    page={restPageData}
+                    searchParams={searchParams}
+                  />
+                </div>
+              </ErrorBoundary>
+            )
+          })}
+      </main>
+    </>
+  )
+}
