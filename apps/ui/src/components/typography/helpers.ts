e9d9fdc2ccdf2aded1c38ad5e72456014252f163
Libor Říha <135842234+liborriha-notum@users.noreply.github.com>
2026-02-24 15:59:51 +0100

    feat: introduce Element component (#249)
    * feat: introduce Element component
    
    * feat: change folder structure
    
    * docs: revert name of the folder
    
    ---------
    
    Co-authored-by: Tomas Jakubek <tmsjkbk@gmail.com>

---
+++ apps/ui/src/components/typography/helpers.ts
@@ -0,0 +1,39 @@
+import {
+  type FontWeight,
+  type ElementTag,
+  type TypographyTag,
+  type TextColor,
+  type Variant,
+  defaultStyles,
+  fontWeightVariants,
+  textColorVariants,
+  variantStyles,
+} from "@/components/typography/config"
+import { cn } from "@/lib/styles"
+
+type HtmlClassNameParams = {
+  tag: ElementTag | TypographyTag
+  variant?: Variant
+  textColor: TextColor
+  fontWeight: FontWeight
+  className?: string
+}
+
+export function resolveHtmlComponentClassName({
+  tag,
+  variant,
+  textColor,
+  fontWeight,
+  className,
+}: HtmlClassNameParams) {
+  const selectedVariant = variant
+    ? variantStyles[variant]
+    : variantStyles[defaultStyles[tag]]
+
+  return cn(
+    selectedVariant,
+    textColorVariants[textColor],
+    fontWeightVariants[fontWeight],
+    className
+  )
+}
