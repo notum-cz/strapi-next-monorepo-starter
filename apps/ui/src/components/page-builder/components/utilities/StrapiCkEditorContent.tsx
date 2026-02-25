5c4c0570782cc158347e20ec3aa2b0b425434e89
lucielandsmanova <landsmanoval@gmail.com>
2026-02-25 10:56:35 +0100

    feat: export config and locale strapi

--- apps/ui/src/components/page-builder/components/utilities/StrapiCkEditorContent.tsx
+++ apps/ui/src/components/page-builder/components/utilities/StrapiCkEditorContent.tsx
@@ -1,6 +1,7 @@
 import type { Data } from "@repo/strapi-types"
 
 import CKEditorRenderer from "@/components/elementary/ck-editor"
+import { Container } from "@/components/elementary/Container"
 import { removeThisWhenYouNeedMe } from "@/lib/general-helpers"
 import type { PageBuilderComponentProps } from "@/types/general"
 
@@ -12,10 +13,12 @@ export function StrapiCkEditorContent({
   removeThisWhenYouNeedMe("StrapiCkEditorContent")
 
   return (
-    <CKEditorRenderer
-      htmlContent={component.content}
-      className="mx-auto w-full max-w-324 px-4 py-8 lg:py-12"
-    />
+    <Container>
+      <CKEditorRenderer
+        htmlContent={component.content}
+        className="mx-auto w-full max-w-324 px-4 py-8 lg:py-12"
+      />
+    </Container>
   )
 }
 
