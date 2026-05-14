f61c46d31dc7983d25659ed986303183de94c5f6
Tomas Jakubek <tmsjkbk@gmail.com>
2026-05-14 21:11:30 +0300

    fix(semantic-release-config): treat security commits as patch releases

--- packages/semantic-release-config/index.js
+++ packages/semantic-release-config/index.js
@@ -1,7 +1,15 @@
 module.exports = {
   branches: ["main"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          { type: "security", release: "patch" },
+          { type: "chore", scope: "deps", release: "patch" },
+        ],
+      },
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
   ],
