0c18cbf8241ef871601bebe659f056b114538e6a
mCesnarik <marcel.cesnarik@notum.cz>
2026-05-15 15:18:36 +0200

    chore: remove invert debug filter from visual spec

--- qa/tests/playwright/visual/visual.spec.ts
+++ qa/tests/playwright/visual/visual.spec.ts
@@ -73,7 +73,6 @@ test.describe("Visual Regression", () => {
           #logo-carousel { visibility: hidden !important; }
           ::-webkit-scrollbar { display: none !important; }
           * { scrollbar-width: none !important; }
-          body { filter: invert(1) !important; }
         `,
       })
 
